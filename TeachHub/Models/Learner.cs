4a98104b8566def5902613b26c73d30620502c19
MayankMeghani <120728455+MayankMeghani@users.noreply.github.com>
2024-10-14 15:21:51 +0530

    initial commit

---
+++ TeachHub/Models/Learner.cs
@@ -0,0 +1,11 @@
+﻿namespace TeachHub.Models
+{
+    public class Learner
+    {
+        public int LearnerId { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public IEnumerable<Review>? Reviews { get; set; } // Navigation property for Reviews
+        public IEnumerable<Enrollment> ?Enrollments { get; set; }
+    }
+}
---
+++ TeachHub/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeachHub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string? ProfilePicture { get; set; }
+
+
+        [Required]
+        public UserRole Role {  get; set; } 
+        public enum UserRole
+        {
+            Teacher = 1,
+            Learner = 2
+        }
+
+
+    }
+}
